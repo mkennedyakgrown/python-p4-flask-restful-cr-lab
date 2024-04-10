4532417f55d8177863408cc58d89f48e7e70a1b2
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-04-10 18:07:11 -0500

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -17,10 +17,27 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        return make_response([plant.to_dict() for plant in Plant.query.all()], 200)
+    
+    def post(self):
+        json = request.get_json()
+        new_plant = Plant(
+            name = json['name'],
+            image = json['image'],
+            price = json['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        return make_response(Plant.query.filter_by(id=id).first().to_dict(), 200)
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
